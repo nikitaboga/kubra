a249ac48f644cc2448de7d05aed1e677d3a6ac17
nikitaboga <155196360+nikitaboga@users.noreply.github.com>
2024-01-02 01:04:03 +0200

    Update azure_upload.py

--- kubra/youtube-watcher/azure_upload.py
+++ kubra/youtube-watcher/azure_upload.py
@@ -1,10 +1,7 @@
 from azure.storage.blob import BlobServiceClient
 from config import config
 
-# storage_acc_key = 'Z98dWOhWG09lB/Hqe+kECwVSCBp/tvv+G+748fDKVE39XV9/GaxiDKlg35IRUdQDR6aBFK8qagdX+ASt+MkAVw=='
-# storage_acc_name = 'stkubra'
-# conn_str = 'DefaultEndpointsProtocol=https;AccountName=stkubra;AccountKey=tTOG21csXypXa4kiOyabBmLhnqeRJkWvyVWetxYdcI1sPpalAAtyYQUuZW3kecztaavO8yZUvppR+ASt9V3d6Q==;EndpointSuffix=core.windows.net'
-# container_name = 'kubra/raw/'
+
 storage_acc_key = config['azure']['storage_acc_key']
 storage_acc_name = config['azure']['storage_acc_name']
 conn_str = config['azure']['conn_str']
